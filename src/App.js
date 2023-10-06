0254c886e085cffc07cc031a4583705aec2e1a1a
Ujjwal Suman <ujjwalsuman007@gmail.com>
2023-10-06 18:11:05 +0530

    commit2
    We can move to other pages through various link in home page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
-import Header from "./components/Header";
-import Blogs from "./components/Blogs";
-import Pagination from "./components/Pagination";
 import { useContext, useEffect } from "react";
 import { AppContext } from "./context/AppContext";
 import "./App.css"
+import { Route, Routes } from "react-router-dom";
+import { useSearchParams,useLocation } from "react-router-dom";
+import Home from "./pages/Home";
+import BlogPage from "./pages/BlogPage";
+import TagPage from "./pages/TagPage";
+import Category from "./pages/CategoryPage"
 
 export default function App() {
   const {fetchBlogPosts} = useContext(AppContext);
 
+  const [searchParams, setSearchParams] = useSearchParams();
+  const location = useLocation();
+
   useEffect(() => {
-    fetchBlogPosts();
-  },[]);
+    const page = searchParams.get("page") ?? 1;
+
+    if(location.pathname.includes("tags")){
+      const tag = location.pathname.split("/").at(-1).replaceAll("-", " ");
+      fetchBlogPosts(Number(page), tag);
+    }
+    else if(location.pathname.includes("categories")) {
+      const category = location.pathname.split("/").at(-1).replaceAll("-", " ");
+      fetchBlogPosts(Number(page), null, category);
+    }
+    else {
+      fetchBlogPosts(Number(page));
+    }
+      
+  },[location.pathname, location.search]);
 
   return (
-    <div className="w-full h-full flex flex-col gap-y-1 justify-center items-center">
-      <Header />
-      <Blogs />
-      <Pagination />
-    </div>
+     
+    <Routes>
+       <Route path="/" element={<Home/>} />
+       <Route path="/blog/blogId" element={<BlogPage/>} />
+       <Route path="/tags/:tag" element={<TagPage/>} />
+       <Route path="/categories/:category" element={<Category/>} />
+    </Routes>
+    // <div className="w-full h-full flex flex-col gap-y-1 justify-center items-center">
+    //   <Header />
+    //   <Blogs />
+    //   <Pagination />
+    // </div>
   );
 
 }
